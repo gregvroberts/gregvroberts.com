4499507f7220f0a3e98bf82d0592fe3725154c6f
Greg Roberts <gregvroberts@outlook.com>
2021-06-02 15:17:36 -0500

    Created first web server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+/**
+ * @Description:
+ * @param w
+ * @param r
+ */
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Print("Hello, world!")
+	/**
+	 * @Description: No matter what path they give us, send us to the handlerFunc
+	 * @param "/" sets the file pattern
+	 * @param handlerFunc call this method everytime this pattern is requested
+	 */
+	http.HandleFunc("/", handlerFunc)
+
+	/**
+	 * @Description: Start the server
+	 * @param ":3000" Port/address
+	 * @param nil use the built in serve functionality of the http framework
+	 */
+	http.ListenAndServe(":3000", nil)
+
 }
